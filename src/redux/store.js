e062a87a0feb55a3d84ead172bfe8f867f88bb4e
Ege Gündüz <ege.gunduz@param.com.tr>
2024-09-12 14:45:29 +0300

    initial saga setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
 import { loadingOverlaySlice } from "./loadingOverlaySlice";
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+applyMiddleware(sagaMiddleware);
 
 const store = configureStore({
   reducer: {
     [loadingOverlaySlice.name]: loadingOverlaySlice.reducer,
   },
+  middleware: getDefaultMiddleware => getDefaultMiddleware({}).concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
